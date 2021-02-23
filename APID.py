c33203323c95584eb9bd07792a11dd78189ac064
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-02-23 23:46:05 +0000

    Use this file to import API variables into scraper. Please copy in your own API key.

--- APID.py
+++ APID.py
@@ -1,2 +1,5 @@
 ##This file exists to inport the variables, such as your API ID, into the Dublin Bikes scraper.
 ##Please do not alter this version. It exists so everyone can copy their own variables into a copy of their own.
+APIKEY = ""
+NAME = "Dublin"
+STATIONS = "https://api.jcdecaux.com/vls/v1/stations"
