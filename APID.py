f64cf4b9648c09bf66abc025dd8450cb3882ddbb
Eoin <71882165+Eoinoha@users.noreply.github.com>
2021-02-23 23:37:11 +0000

    Add files via upload

---
+++ APID.py
@@ -0,0 +1,2 @@
+##This file exists to inport the variables, such as your API ID, into the Dublin Bikes scraper.
+##Please do not alter this version. It exists so everyone can copy their own variables into a copy of their own.
