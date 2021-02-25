a6a487f6996b0c7bcb3d04370d162b387acf1ffb
Sonia Szchniewicz <sylwia.szachniewicz@ucdconnect.ie>
2021-02-25 19:14:49 +0000

    Updated to only create availability table if one doesn't exist and removed stations table

--- jcdecaux_api_scraper.py
+++ jcdecaux_api_scraper.py
@@ -1,5 +1,4 @@
-#Importing packages
-
+# Importing packages
 from sqlalchemy import Table, Column, Integer, Float, String, MetaData, DateTime, create_engine
 from sqlalchemy.orm import sessionmaker
 import requests
@@ -8,30 +7,12 @@ import datetime
 from IPython.display import JSON
 import time
 
-#Connecting to databse
+# Import API details and database connection from APID.
+# Make sure APID has your details or it will fail.
+from APID import *
 
-engine = create_engine("mysql+mysqlconnector://admin:dublinbikes@dublinbikes.ciwb2rbkjl8e.us-east-1.rds.amazonaws.com:3306/dublinbikes", echo=True)
-
-#Creating tables
-
+# Creating table
 meta = MetaData()
-
-stations = Table(
-    'stations', meta,
-    Column('number', Integer, primary_key = True),
-    Column('contract_name', String(128)),
-    Column('name', String(128)),
-    Column('address', String(128)),
-    Column('pos_lat', Float),
-    Column('pos_lng', Float),
-    Column('banking', Integer),
-    Column('bonus', Integer),
-    Column('bike_stands', Integer),
-    Column('available_bike_stands', Integer),
-    Column('available_bikes', Integer),
-    Column('status', String(128)),
-    Column('last_update', DateTime)
-)
 
 availability = Table(
     'availability', meta,
@@ -47,51 +28,18 @@ availability = Table(
     Column('available_bike_stands', Integer),
     Column('available_bikes', Integer),
     Column('status', String(128)),
-    Column('last_update', DateTime)
+    Column('last_update', DateTime),
+    Column('post_time', DateTime)
 )
-try:
-    stations.drop(engine)
-    availability.drop(engine)
-except:
-    pass
-meta.create_all(engine)
+availability.create(engine, checkfirst=True)
 
-
-#Connecting to JCDecaux API
-
-APIKEY = "541e273750ef405656887b8db71e95bcd8652a47"
-NAME = "Dublin"
-STATIONS = "https://api.jcdecaux.com/vls/v1/stations"
+# Connecting to JCDecaux API
+from APID import *
 
 r = requests.get(STATIONS, params={"apiKey": APIKEY, "contract": NAME})
 
 
-#Inserting data into the stations table
-
-def get_station(obj):
-    return {'number': obj['number'],
-            'contract_name': obj['contract_name'],
-           'name': obj['name'],
-            'address': obj['address'],
-            'pos_lng': obj['position']['lng'],
-            'pos_lat': obj['position']['lat'],
-            'banking': obj['banking'],
-            'bonus': obj['bonus'],
-            'bike_stands': obj['bike_stands'],
-            'available_bike_stands': obj['available_bike_stands'],
-            'available_bikes': obj['available_bikes'],
-            'status': obj['status'],
-            'last_update': datetime.datetime.fromtimestamp( int(obj['last_update'] / 1e3) )
-           }
-
-values = list(map(get_station, r.json()))
-#print(values)
-ins = stations.insert().values(values)
-engine.execute(ins)
-
-
 # Inserting data into the availability table every 5 minutes
-
 def main():
     while True:
         try:
@@ -110,8 +58,15 @@ def main():
                         'available_bike_stands': obj['available_bike_stands'],
                         'available_bikes': obj['available_bikes'],
                         'status': obj['status'],
-                        'last_update': datetime.datetime.fromtimestamp(int(obj['last_update'] / 1e3))
+                        'last_update': datetime.datetime.fromtimestamp(int(anti_none(obj['last_update']) / 1e3)),
+                        'post_time': datetime.datetime.now()
                         }
+
+            # This Function just sets the value to 0 if it is None. This prevents crashes if there is an issue with the API.
+            def anti_none(result):
+                if result is None:
+                    result = 0
+                return result
 
             values = list(map(get_availability, r.json()))
             ins = availability.insert().values(values)
@@ -126,3 +81,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
