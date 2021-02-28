ccd27fbf6f3b8f8cc74222288b7d571f47eee00b
MatthewKehoeUCD <matthew.kehoe1@ucdconnect.ie>
2021-02-28 16:54:31 +0000

    further functionalization of weather scraper

--- accuweather_api_scraper.py
+++ accuweather_api_scraper.py
@@ -36,6 +36,22 @@ def get_weather(obj):
             'Link': obj['Link']
            }
 
+
+
+def store (request_result):
+    """
+    1. Maps the JSON object to returned from Accuweather API to a list
+    2. Creates an insertion instruction to be passed to the engine execute method
+    3. Runs execution method of the engine on the insertion instruction
+
+    :param request_result: JSON request result pulled from accuweather API
+    :return: None, as function simple executes the insert instruction to the RDS
+    """
+    values = list(map(get_weather, request_result.json()))
+    ins = weather.insert().values(values)
+    engine.execute(ins)
+    return
+
 #Connecting to databse
 
 engine = create_engine("mysql+mysqlconnector://admin:admin1234@dbikes.ccecuvqpo8jx.us-east-1.rds.amazonaws.com:3306/dbikes", echo=True)
@@ -67,7 +83,7 @@ except:
 meta.create_all(engine)
 
 
-#Connecting to Accuweather API
+#Connecting to Accuweather API --> NEEDS TO BE SETUP FOR ENVIROMENT VARIABLES
 
 APIKEY = "v7sO8DZEJ6XOtWZmAhoG9Ikv2XQTmnro"
 LOCATION = "207931"
@@ -75,11 +91,7 @@ RESOURCEURL = "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/"
 
 #Inserting data into the weather table
 
-# def store (request_result):
-#     values = list(map(get_weather, request_result.json()))
-#     ins = weather.insert().values(values)
-#     engine.execute(ins)
-#     return
+
 
 
 
@@ -88,18 +100,22 @@ def main():
     # infinite loop to enable continuous data collection
     while True:
         try:
-            #sending get request to specified URL
+            # #sending get request to specified URL
             r = requests.get(f"{RESOURCEURL}{LOCATION}?apikey={APIKEY}")
-            print(r.json())
+            store(r)
 
-            #storing the values from returned json to a list
-            values = list(map(get_weather, r.json()))
+            #keeping non functionalized code in case of error as I am sure it works
+            # print(r.json())
+            #
+            # #storing the values from returned json to a list
+            # values = list(map(get_weather, r.json()))
+            #
+            # #creating insert command for sqlalchemy engine
+            # ins = weather.insert().values(values)
+            #
+            # #executing insert command
+            # engine.execute(ins)
 
-            #creating insert command for sqlalchemy engine
-            ins = weather.insert().values(values)
-
-            #executing insert command
-            engine.execute(ins)
 
             #repeat every hour
             time.sleep(60 * 60)
