c907c020ca5e9aecedc17ee70275a536ca10137e
MatthewKehoeUCD <matthew.kehoe1@ucdconnect.ie>
2021-03-04 10:15:44 +0000

    Added doc string and comments

--- jcdecaux_api_scraper.py
+++ jcdecaux_api_scraper.py
@@ -1,3 +1,11 @@
+"""
+Scraper to query JCDecaux Dublin bike API for station availability data.
+ 1. Creates a table with headers
+ 2. Creates mysqlAlchemy engine and
+ 3. Runs query periodically (every 5 mins) Requests information from dublin bikes API
+ 4. Converts information into a DB friendly format and populates availability rows
+"""
+
 # Importing packages
 from sqlalchemy import Table, Column, Integer, Float, String, MetaData, DateTime, create_engine
 from sqlalchemy.orm import sessionmaker
@@ -46,6 +54,13 @@ def main():
             r = requests.get(STATIONS, params={"apiKey": APIKEY, "contract": NAME})
 
             def get_availability(obj):
+                """
+                Takes the API JSON object retrieved from the JCDecaux API request and returns a python dictionary to
+                facilitate further commands and manipulations.
+                :param obj: JCDecAPI JSON request object
+                :return: dictionary mapping each JSON object to relevant key header
+                """
+
                 return {'number': obj['number'],
                         'contract_name': obj['contract_name'],
                         'name': obj['name'],
@@ -62,14 +77,32 @@ def main():
                         'post_time': datetime.datetime.now()
                         }
 
-            # This Function just sets the value to 0 if it is None. This prevents crashes if there is an issue with the API.
             def anti_none(result):
+                """
+                Prevents crashes if there is an issue with the API
+
+
+                1. Takes a result object and checks if value is none
+                    a. if so, sets value to 0
+                    b. otherwise does nothing
+
+                :param result: API request return object
+                :return: returns result variable
+                """
+
+
                 if result is None:
                     result = 0
                 return result
 
+
+            #mapping values from the get_availability dict to a list
             values = list(map(get_availability, r.json()))
+
+            #creatign insertion instruction set for engine
             ins = availability.insert().values(values)
+
+            #executing the insertion instuctions
             engine.execute(ins)
 
             time.sleep(5 * 60)
