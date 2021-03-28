7029fffad8ab0276f6d492e2ff6845fc1c004e13
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-03-28 03:47:15 +0100

    expanded weather widget

--- flaskapp/static/weather.js
+++ flaskapp/static/weather.js
@@ -8,9 +8,30 @@ function initList() {
     }).then(data => {
       console.log("data: ", data);
 
-    result = "<div style='text-align: left;'><h3>  Current Weather</h3><iframe src='https://free.timeanddate.com/clock/i7qk59en/n78/tlie/tt0/tw1/tm1' frameborder='0' width='182' height='18'></iframe><p>----------------------------------------------</p>";
+    result = "<div style='text-align: left;'><h2>  Current Weather</h2><iframe src='https://free.timeanddate.com/clock/i7qk59en/n78/tlie/tt0/tw1/tm1' frameborder='0' width='182' height='18'></iframe>";
+
+    result += "<table id='weaterTable'></table>"
+
     data.forEach(weather => {
-      result += "<p>Temperature: " + weather.Temperature + "ºC</p>";
+      var temp = weather.Temperature
+      var RFTemp = weather.RealFeelTemperature
+      var icon = weather.WeatherIcon
+      var iconPic = "<img class='loading' src='static/weatherIcons/"+icon+".gif'>";
+      var iconPhrase = weather.IconPhrase
+      var pp = weather.PrecipitationProbability
+      var rain = weather.Rain
+      var humid = weather.RelativeHumidity
+      var wind = weather.WindSpeed
+
+      result += "<p>Temperature: " + temp + "ºC</p>";
+      result += "<p>Real Feel Temperature: " + RFTemp + "ºC</p>";
+      result += iconPic;
+      result += iconPhrase;
+      result += "<p>Icon: " + icon + "</p>";
+      result += "<p>Precipitation Probability: " + pp + "</p>";
+      result += "<p>Rain: " + rain + "</p>";
+      result += "<p>Relative Humidity: " + humid + "</p>";
+      result += "<p>Wind Speed: " + wind + "</p>";
     });
     
     result += "</div>"
