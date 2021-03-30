aca7c5b134efcc12c1003ae1468b9be78aa1df1e
MatthewKehoeUCD <matthew.kehoe1@ucdconnect.ie>
2021-03-30 10:48:54 +0100

    deleted old files

--- APID.py
+++ APID.py
@@ -3,10 +3,16 @@
 
 from sqlalchemy import Table, Column, Integer, Float, String, MetaData, DateTime, create_engine
 
-engine = create_engine("mysql+mysqlconnector://admin:admin1234@dublinbikes.co2l5qvrgqx0.us-east-1.rds.amazonaws.com:3306/DublinBikes", echo=True)
+A = "admin"
+B = "admin1234"
+C = "dbikes.ccecuvqpo8jx.us-east-1.rds.amazonaws.com"
+D = "3306"
+E = "dbikes"
 
 
-APIKEY = "04292f59ce3536eb87c92523bbc4cfcb871b3c8f"
+engine = create_engine(f"mysql+mysqlconnector://{A}:{B}@{C}:{D}/{E}", echo=True)
+
+APIKEY = "9514a54c5679aff7255ccc24632f77b9f319a7f8"
 NAME = "Dublin"
 STATIONS = "https://api.jcdecaux.com/vls/v1/stations"
 
