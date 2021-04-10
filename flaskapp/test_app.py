f18c9f444135ee4bdb0cf4c43a4ef54e1d2239ee
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-04-10 19:08:21 +0100

    added tests for accessing all three pages of the app

--- flaskapp/test_app.py
+++ flaskapp/test_app.py
@@ -6,6 +6,10 @@ except Exception as e:
 
 class FlaskTest(unittest.TestCase):
 
+
+
+    ## Index tests
+
     def test_index(self):
         tester = app.test_client(self)
 
@@ -19,6 +23,32 @@ class FlaskTest(unittest.TestCase):
         response = tester.get("/home")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
+
+
+
+    ## Map tests
+
+    def test_map(self):
+        tester = app.test_client(self)
+
+        response = tester.get("/map")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+
+
+    ## Stations tests
+
+    def test_stations(self):
+        tester = app.test_client(self)
+
+        response = tester.get("/stations")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+
+
+
         
 if __name__ == "__main__":
     unittest.main()
