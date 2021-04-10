ff3c52b40ea4ef858a4b57a3800277e79f2f681b
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-04-10 19:00:34 +0100

    added test for second route to index

--- flaskapp/test_app.py
+++ flaskapp/test_app.py
@@ -9,10 +9,16 @@ class FlaskTest(unittest.TestCase):
     def test_index(self):
         tester = app.test_client(self)
 
-        response = tester.get("/home")
+        response = tester.get("/")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
         
+    def test_index2(self):
+        tester = app.test_client(self)
+
+        response = tester.get("/home")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
         
 if __name__ == "__main__":
     unittest.main()
