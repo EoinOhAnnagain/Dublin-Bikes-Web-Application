91294a6d77afaa20c06d35a77fc675173ea09e94
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-04-11 00:59:47 +0100

    comments and docstrings added to the tests.

--- flaskapp/test_app.py
+++ flaskapp/test_app.py
@@ -7,10 +7,15 @@ except Exception as e:
 class FlaskTest(unittest.TestCase):
 
 
-
+    ## Tests on pages
+    ## This section contains tests on the pages that make up the web app.
+    ## Please note I have only provided docstrings and comments on the ones related to index as the map and stations ones are functionally the same.
+    
     ## Index tests
 
     def test_index(self):
+        '''This test checks that the correct status code is returned when accessing the index page.
+            It will fail if the status code returned is not 200'''
         tester = app.test_client(self)
         response = tester.get("/")
 
@@ -18,6 +23,8 @@ class FlaskTest(unittest.TestCase):
         self.assertEqual(statuscode, 200)
         
     def test_index2(self):
+        '''This is a second test on the status code for index using its second access point of /home
+            This route is not checked for the remaining tests on index as if both as successful then the remaining tests will have identical responces'''
         tester = app.test_client(self)
         response = tester.get("/home")
 
@@ -25,17 +32,22 @@ class FlaskTest(unittest.TestCase):
         self.assertEqual(statuscode, 200)
 
     def test_index_content(self):
+        '''Check on the type of content that is returned.
+            Expected return is text/html; charset=utf-8'''
         tester = app.test_client(self)
         response = tester.get("/")
 
         self.assertEqual(response.content_type, "text/html; charset=utf-8")
 
     def test_index_data(self):
+        '''Check on the data returned. Makes several check.'''
         tester = app.test_client(self)
         response = tester.get("/")
 
-        self.assertTrue(b'Home Page' in response.data)
-        self.assertTrue(b'Home' in response.data)
+        self.assertTrue(b'Home Page' in response.data) ## Check that the correct page title is returned.
+        self.assertTrue(b'Home' in response.data) ## check that the correct page name is returned.
+        
+        ## The following checks are for the opening and closing html tags. These are provided by base.html with index extends. If they fail then the pages are not connecting correctly. 
         self.assertTrue(b'<html>' in response.data)
         self.assertTrue(b'</html>' in response.data)
 
