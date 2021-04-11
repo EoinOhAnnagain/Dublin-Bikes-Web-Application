ca9851e238b28425e6247383fe6ce843086865d6
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-04-11 20:34:31 +0100

    just checking nothing needs to be pushed

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -83,25 +83,6 @@ def stationsquery():
     #print([res for res in results])
     return df.to_json(orient='records')
 
-<<<<<<< HEAD
-@app.route("/occupancy/<int:station_id>")
-@lru_cache # temporarily cache result
-def get_occupancy(station_id):
-    sql = f"""
-        SELECT number, last_update, available_bike_stands, available_bikes, name, post_time FROM availability 
-        where number = {station_id}
-    """
-    df = pd.read_sql_query(sql, engine)
-    df = df[(df['last_update'].dt.year != 1970)]
-    res_df = df.set_index('last_update').resample('1d').mean()
-    res_df['name'] = df['name'][0]
-    res_df['last_update'] = res_df.index
-    res_df = res_df.tail(32)
-    return res_df.to_json(orient='records')
-
-
-=======
->>>>>>> dev/Eoin
 
 
     
